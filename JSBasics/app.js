506c727c7efd05430cd0a9b423e7f65de0da6563
sarikaraj11 <sarikasunilraj@gmail.com>
2025-04-15 20:12:35 +1200

    express and app.js

---
+++ JSBasics/app.js
@@ -0,0 +1,19 @@
+console.log("My app is Start here");
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+//app.get('/', (req, res) => {
+  //res.send('Hello World!')
+//})
+
+app.get("/", (req, res, next) => {
+    console.log("I received a get request on the path // ")
+    res.send("<h1>Acknowledged</h1>")
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ JSBasics/jsf.js
@@ -0,0 +1,8 @@
+let multiple=function fun1(p1,p2){
+return  p1*p2;
+}
+//console.log("MY Function 1 is=",fun1(10,20));
+console.log("i used variable to transfer mutiple here",multiple);
+
+let multiple1=(p1,p2)=>p1*p2; 
+console.log(multiple1(20,30));
